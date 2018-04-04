c060909844dd2eecdbfbae7589bc4e56d182dadd
sumshetty <sumukhshetty6@gmail.com>
2018-04-04 13:34:28 +0530

    Wrote tests for approve and reject. Built the functions around them in the contract

--- contracts/gruntFund.sol
+++ contracts/gruntFund.sol
@@ -6,22 +6,22 @@ pragma solidity ^0.4.0 ;
 
 contract GruntFund {
     struct Deliverable {
-            address createdBy;
-            string description;
-            uint id;
-            uint approvals;
-            mapping (address => bool) moderators;
-            bool completed;
-            
-        }
+        address createdBy;
+        string description;
+        uint id;
+        uint approvals;
+        uint rejects;
+        mapping (address => bool) voted;
+        bool completed;
+        bool rejected;
+    }
 
 
-        function ProjectGrunt () public {
-      
-       
+    function GruntFund () public {
         totalTimeList.push(msg.sender);
-        totalTime.[msg.sender] = 1;
-   }
+        totalTime[address(this)] = 1;
+        totalTime[msg.sender] = 1;
+    }
 
     mapping (uint => Deliverable) public pendingDeliverables;
     uint[] public pendingDeliverableList;
@@ -30,44 +30,64 @@ contract GruntFund {
     address[] public totalTimeList;
 
     function getVersion() public pure returns (string) {
-        return 'version 0.0.4';
+        return "version 0.0.4";
     }
 
     function getPendingDeliverableList() public view returns (uint[]) {
         return pendingDeliverableList;
     }
 
-    function getPendingDeliverable(uint _id) public view returns (address, string, uint, uint, bool) {
+    function getPendingDeliverable(uint _id) public view returns (address, string, uint, uint, bool, uint, bool) {
         return (
         pendingDeliverables[_id].createdBy,
         pendingDeliverables[_id].description,
         pendingDeliverables[_id].id,
         pendingDeliverables[_id].approvals,
-        pendingDeliverables[_id].completed
+        pendingDeliverables[_id].completed,
+        pendingDeliverables[_id].rejects,
+        pendingDeliverables[_id].rejected
+
         );
     }
     
     function addDeliverable(string _description) public {
-        uint id = now;
+        uint id = block.timestamp;
         Deliverable memory newTask = Deliverable({
             createdBy: msg.sender,
             description: _description,
             id: id,
             approvals: 0,
-            completed: false
+            rejects: 0,
+            completed: false, 
+            rejected: false
+
         });
         pendingDeliverables[id] = newTask;
         pendingDeliverableList.push(id);
     }
-
+     /// @author Sumukh
+    /// @notice Approving based on weight. 3 requires allow to check if the person voting is a grunt, if the task is completed and if has already voted.
+    /// @return true if Bugs will eat it, false otherwise
     function approveDeliverable(uint _deliverableId) public returns(address) {
         
         Deliverable storage existingTask = pendingDeliverables[_deliverableId];
-        require(!totalTime[exisitingTask.createdBy]>=1);
-        require(!existingTask.completed);
-        require(!existingTask.moderators[msg.sender]);
+        require(totalTime[msg.sender]>=1);
+        require(!existingTask.completed && !existingTask.rejected);
+        require(!existingTask.voted[msg.sender]);
         existingTask.approvals += totalTime[msg.sender];
-        existingTask.moderators[msg.sender] = true;
+        existingTask.voted[msg.sender] = true;
+        pendingDeliverables[_deliverableId] = existingTask;
+        finalise(_deliverableId,  existingTask.createdBy);
+        return existingTask.createdBy;
+    }
+    function rejectDeliverable(uint _deliverableId) public returns(address) {
+        
+        Deliverable storage existingTask = pendingDeliverables[_deliverableId];
+        require(totalTime[msg.sender]>=1);
+        require(!existingTask.completed && !existingTask.rejected);
+        require(!existingTask.voted[msg.sender]);
+        existingTask.rejects += totalTime[msg.sender];
+        existingTask.voted[msg.sender] = true;
         pendingDeliverables[_deliverableId] = existingTask;
         finalise(_deliverableId,  existingTask.createdBy);
         return existingTask.createdBy;
@@ -79,6 +99,8 @@ contract GruntFund {
             totalTime[_createdBy] += 2;
             totalTimeList.push(_createdBy);
             pendingDeliverables[_deliverableId].completed = true;
+        } else if (pendingDeliverables[_deliverableId].rejects > (totalTime[address(this)]/2)) {
+            pendingDeliverables[_deliverableId].rejected = true;
         }
     }
 
@@ -86,12 +108,11 @@ contract GruntFund {
         return totalTimeList;
     }
 
-  function getSummary(address _id) public view returns (uint) {
+    function getSummary(address _id) public view returns (uint) {
         return totalTime[_id];
     }
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
-
 }
